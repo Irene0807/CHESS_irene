cdb1ae51e978bf3d4ed68ac6a78c196d3ea0d960
I.r.e.n.e <110386804+Irene0807@users.noreply.github.com>
2023-06-20 21:13:01 +0800

    Final Update 2

--- src/player/submission.cpp
+++ src/player/submission.cpp
@@ -3,7 +3,7 @@
 
 #include "../config.hpp"
 #include "../state/state.hpp"
-#include "../policy/random.hpp"
+#include "../policy/submission.hpp"
 
 
 State* root;
@@ -40,15 +40,17 @@ void read_board(std::ifstream& fin) {
  */
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
+  int depth = 4;
   while(true) {
-    // Choose a random spot.
-    auto move = Random::get_move(root, 0);
+    // Choose a minimax spot.
+    auto move = Alphabeta::get_move(root, depth);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
     // Remember to flush the output to ensure the last action is written to file.
     fout.flush();
-    break;
+    depth += 1;
+
   }
 }
 
--- src/policy/submission.hpp
+++ src/policy/submission.hpp
@@ -6,7 +6,8 @@
  * @brief Policy class for random policy, 
  * your policy class should have get_move method
  */
-class Random{
+class Alphabeta{
 public:
   static Move get_move(State *state, int depth);
+  static int alphabeta(State *state, int depth, int alpha, int beta, bool isminimax);
 };
